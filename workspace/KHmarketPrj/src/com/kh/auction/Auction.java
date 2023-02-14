e901c961282ae8e324e46962703e1194bdd6047e
kms6210 <93501719+kms6210@users.noreply.github.com>
2023-02-15 01:19:49 +0900

    kms6210 (#9)

---
+++ workspace/KHmarketPrj/src/com/kh/auction/Auction.java
@@ -0,0 +1,15 @@
+package com.kh.auction;
+
+// 테이블 : 경매, 경매 입찰 
+
+public class Auction {
+	public void bid() {
+		// 입찰하기 
+	}
+	
+	public void closeBid() {
+		// 입찰 취소 (ex. 대신 수수료 발생)
+	}
+	
+	
+}
