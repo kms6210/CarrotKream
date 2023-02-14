0f93623756adaf6811e81f86dbe076caf84267f4
DESKTOP-NOGAGFD\user <kms2786210@naver.com>
2023-02-15 01:05:54 +0900

    no message

---
+++ workspace/KHmarketPrj/src/com/kh/trade/InteractItem.java
@@ -0,0 +1,25 @@
+package com.kh.trade;
+
+// 테이블 : 좋아요 목록, 거래 후기, 상품
+
+public class InteractItem {
+	public void selectLike() {
+		// 좋아요(찜) 하기 
+	}
+	
+	public void deleteLike() {
+		// 좋아요(찜) 삭제하기 
+	}
+	
+	public void writeReview() {
+		// 거래 후기 작성 
+	}
+	
+	public void sellItem() {
+		// 판매
+	}
+	
+	public void buyItem() {
+		// 구매 
+	}
+}
