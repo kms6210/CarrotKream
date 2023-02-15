b82135236c36ccd80f51c13f76aea8ce0c74bd96
kms6210 <93501719+kms6210@users.noreply.github.com>
2023-02-15 15:22:18 +0900

    commit (#11)
    2/15 15:22

---
+++ workspace/KHmarketPrj/src/com/kh/mutualAction/MutualAction.java
@@ -0,0 +1,33 @@
+package com.kh.mutualAction;
+
+// 테이블 : 좋아요 목록, 거래 후기, 상품, 채팅방, 채팅 내용 
+
+public class MutualAction {
+	public void setLike() {
+		// 좋아요(찜) 하기 
+	}
+	
+	public void deleteLike() {
+		// 좋아요(찜) 삭제하기 
+	}
+	
+	public void writeReview() {
+		// 거래 후기 작성 
+	}
+	
+	public void sellItem() {
+		// 판매
+	}
+	
+	public void buyItem() {
+		// 구매 
+	}
+	
+	public void makeChatRoom() {
+		// 채팅방 생성 
+	}
+	
+	public void chat() {
+		// 채팅하기 
+	}
+}
--- workspace/KHmarketPrj/src/com/kh/user/User.java
+++ workspace/KHmarketPrj/src/com/kh/user/User.java
@@ -1,6 +1,6 @@
 package com.kh.user;
 
-// 테이블 : 유저 테이블, 비밀번호 찾기 힌트, 비밀번호 찾기 질문  
+// 테이블 : 유저, 힌트 질문 유형, 제재 내역, Q&A
 
 public class User {
 	public void join() {
@@ -19,19 +19,19 @@ public class User {
 		// 비밀번호 찾기(아이디 & 힌트)
 	}
 	
-	public void joinRule() {
-		// 회원가입시 규칙 (ex. 특수문자포함, 8~15자리 ...)
+	public void applyjoinRule() {
+		// 회원가입 규칙 (ex. 특수문자포함, 8~15자리 ...)
 	}
 	
-	public void judgePwdGrade() {
-		// 회원가입시 비밀번호 보안 등급 판단 (ex. 낮음 중간 높음 ...)
+	public void showPwdGrade() {
+		// 비번 설정시 보안 등급 출력 (ex. 낮음 중간 높음 ...)
 	}
 	
-	public void showHint() {
-		// 아이디, 비밀번호 힌트 질문 보여주기
-	}
-	
-	public void removeUser() {
+	public void dropUser() {
 		// 회원 탈퇴
+	}
+	
+	public void askQuestion() {
+		// 질문하기
 	}
 }
