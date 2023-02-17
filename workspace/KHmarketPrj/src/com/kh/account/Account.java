7347a0b3ecc3cd15373a198e4d9a00214652ed7e
2/10 12:28 기본 셋팅 <>
2023-02-17 15:32:14 +0900

    충전 기능 업데이트 + 휴대폰 char(20)으로 변경

--- workspace/KHmarketPrj/src/com/kh/account/Account.java
+++ workspace/KHmarketPrj/src/com/kh/account/Account.java
@@ -2,6 +2,7 @@ package com.kh.account;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.kh.main.Main;
@@ -9,20 +10,39 @@ import com.kh.main.Main;
 // 테이블 : 유저. 계좌 내역 
 
 public class Account {
-	public void deposit(int user_no, int target_no, Connection conn) throws Exception { 
-		// 충전(유저)
+	public void deposit(int user_no, Connection conn) throws Exception { 
+		// 충전(유저) : 자기 자신에게 입금 
 		System.out.print("금액 : ");
 		int price = Integer.parseInt(Main.SC.nextLine());
-		String sql = "INSERT INTO USER_ACCOUNT(USE_NO, USER_NO, TARGET_NO, PRICE) VALUES(SEQ_USER_USE_NO.NEXTVAL, ?, ?, ?)";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, user_no);
-		pstmt.setInt(2, target_no);
-		pstmt.setInt(3, price);
-		int result = pstmt.executeUpdate();
 		
-		if (result == 1) {
-			System.out.println("충전 완료");
+		String sql1 = "INSERT INTO USER_ACCOUNT(USE_NO, USER_NO, TARGET_NO, PRICE) VALUES(SEQ_USER_USE_NO.NEXTVAL, ?, ?, ?)";
+		PreparedStatement pstmt1 = conn.prepareStatement(sql1);
+		pstmt1.setInt(1, user_no);
+		pstmt1.setInt(2, user_no);
+		pstmt1.setInt(3, price);
+		int result1 = pstmt1.executeUpdate();
+		
+		String sql2 = "SELECT BALANCE FROM K_USER WHERE USER_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql2);
+		pstmt.setInt(1, user_no);
+		ResultSet rs = pstmt.executeQuery();
+		
+		int balance = 0;
+		if(rs.next()) {
+			balance = rs.getInt("BALANCE");
+		}
+		
+		String sql3 = "UPDATE K_USER SET BALANCE = ? WHERE USER_NO = ?";
+		PreparedStatement pstmt3 = conn.prepareStatement(sql3);
+		pstmt3.setInt(1, price + balance);
+		pstmt3.setInt(2, user_no);
+		int result3 = pstmt3.executeUpdate();
+		
+		if (result1 == 1 && result3 == 1) {
+			System.out.println(price + "원 충전 완료!");
 		} else {
+			System.out.println(result1);
+			System.out.println(result3);
 			System.out.println("충전 실패...");
 		}
 	}
