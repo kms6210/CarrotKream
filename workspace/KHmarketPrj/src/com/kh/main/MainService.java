7b0963c707518b59e72bf77609ec3cd4500b6c30
DESKTOP-J3DASVP\user2 <kms2786210@naver.com>
2023-02-17 21:19:07 +0900

    유저 계좌의 충전, 인출, 이체 기능 추가

--- workspace/KHmarketPrj/src/com/kh/main/MainService.java
+++ workspace/KHmarketPrj/src/com/kh/main/MainService.java
@@ -2,6 +2,7 @@ package com.kh.main;
 
 import java.sql.Connection;
 
+import com.kh.account.Account;
 import com.kh.jdbc.JdbcTemplate;
 import com.kh.mutualAction.MutualAction;
 
@@ -11,11 +12,10 @@ public class MainService {
 		showMarket();
 		System.out.print("번호를 입력하세요 : ");
 		String input = Main.SC.nextLine();
-		if(input.equals("9")) {
+		if (input.equals("9")) {
 			System.out.println("프로그램을 종료합니다.");
 			return true;
-		}
-		else {
+		} else {
 			processService(input);
 			return false;
 		}
@@ -23,34 +23,75 @@ public class MainService {
 
 	public void showMarket() {
 		// 정보 보여주기
+		System.out.println("1. 찜하기, 2. 내 계좌 관리");
+	}
+
+	public void processService(String input) throws Exception {
+		Connection conn = JdbcTemplate.m01();
+
+		MutualAction mutualAction = new MutualAction();
+		Account account = new Account();
+
+		switch (input) {
+		case "1":
+			try {
+				mutualAction.setLikeList(conn, 1, 2);
+			} catch (Exception e) {
+				System.out.println("이미 찜한 상품입니다.");
+			}
+			break;
+			
+		case "2":
+			System.out.print("유저 번호를 입력하세요 : "); // 로그인으로 대체해야함
+			int user_no = 0;
+			
+			try {
+				user_no = Integer.parseInt(Main.SC.nextLine());
+				System.out.print("[1. 충전, 2. 인출, 3. 이체] 번호를 선택하세요 : ");
+				String num = Main.SC.nextLine();
+				if (num.equals("1")) {
+					account.deposit(user_no, conn);
+				} else if (num.equals("2")) {
+					account.withdraw(user_no, conn);
+				} else if (num.equals("3")) {
+					System.out.print("상대의 유저 번호를 입력하세요 : ");
+					int target_no = Integer.parseInt(Main.SC.nextLine());
+					account.transfer(user_no, target_no, conn);
+				} else {
+					System.out.println("잘못 입력하셨습니다.");
+				}
+			} catch (Exception e) {
+				System.out.println("숫자를 입력해주세요.");
+			}
+
+			break;
+			
+		case "3":
+			break;
+			
+		case "4":
+			break;
+			
+		case "5":
+			break;
+			
+		default:
+			System.out.println("잘못 입력하셨습니다….");
+		}
+
+		System.out.println("");
+		conn.close();
 	}
 	
 	public void processService() {
 		// 서비스 진행
 	}
-	
+
 	public void showPopularItems() {
 		// 인기 품목 노출
 	}
-	
+
 	public void showFAQ() {
-		// 자주 묻는 질문 출력 
-	}
-	
-	public void processService(String input) throws Exception {
-		Connection conn = JdbcTemplate.m01();
-		
-		MutualAction mutualAction = new MutualAction();
-		switch(input) {
-		case "1" : mutualAction.setLikeList(conn, 1, 1); break;
-		case "2" : break;
-		case "3" : break;
-		case "4" : break;
-		case "5" : break;
-		default : System.out.println("잘못 입력하셨습니다….");
-		}
-		
-		System.out.println("");
-		conn.close();
+		// 자주 묻는 질문 출력
 	}
 }
