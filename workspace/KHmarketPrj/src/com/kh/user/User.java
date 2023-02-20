146a34825424afb2484fcfce695b1daa70074753
ccoldcat <ccoldcat@gmail.com>
2023-02-21 03:40:59 +0900

    0221 몇몇 기능 추가 및 수정
    // 수정 및 구현한 기능
    
    [회원가입] 아이디 글자 길이 제한 규칙 추가하기 + 중복 아이디 검사
    [회원 탈퇴] "1. 네 / 2. 아니오 : [input]" 이런식으로 println이 아닌 print로 변경하기
    [로그아웃] 로그아웃 기능 추가하기
    [질문하기] 4번과 마찬가지로 "질문을 입력해주세요 : [input]" 형식으로 변경하기
    [질문하기] 질문 완료하면 ANSWER = '미답변'이던데 그냥 null
    
    // 작업예정...
    
    [유저페이지] 내가 한 질문 조회/삭제
    [비밀번호 찾기] 전화번호를 010-0000-0000으로 입력하든, 01000000000으로 입력하든 통과되게 변경하기
    [회원가입] 힌트 선택전 무슨 질문들 있는지 보여주기

--- workspace/KHmarketPrj/src/com/kh/user/User.java
+++ workspace/KHmarketPrj/src/com/kh/user/User.java
@@ -14,12 +14,14 @@ public class User {
 	private final UserInput uInput = new UserInput();
 	
 	// 회원 가입
-	public void join(Connection conn) throws Exception {
-		//데이터 입력받기
-		UserData data = uInput.UserJoinInput();
+	public int join(Connection conn) throws Exception {
 		
+		// 데이터 입력받기
+		UserData data = uInput.UserJoinInput(conn);
 		
-		String sql = "INSERT INTO K_USER (USER_NO, ID, PWD, NICK, PHONE_NO, ADDRESS, QUESTION_NO, ANSWER) VALUES (SEQ_USER_NO.NEXTVAL, ?, ?, ?, ?, ?, ?, ?)";
+		// 데이터 처리
+		String sql = "INSERT INTO K_USER (USER_NO, ID, PWD, NICK, PHONE_NO, ADDRESS, QUESTION_NO, ANSWER)"
+													+ " VALUES (SEQ_USER_NO.NEXTVAL, ?, ?, ?, ?, ?, ?, ?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, data.getUserId());
 		pstmt.setString(2, data.getUserPwd());
@@ -29,19 +31,12 @@ public class User {
 		pstmt.setString(6, data.getUserQuestion());
 		pstmt.setString(7, data.getUserAnswer());
 		
-		int result = pstmt.executeUpdate();
-		
-		//결과
-		if(result == 1) {
-			System.out.println("회원가입 성공!");
-		}else {
-			System.out.println("회원가입 실패...");
-		}
-		
+		//리턴
+		return pstmt.executeUpdate();
 	}
 	
 	// 로그인
-	public void login(Connection conn) throws Exception {
+	public int login(Connection conn) throws Exception {
 		
 		//데이터 입력받기
 		UserData data = uInput.UserLoginInput();
@@ -52,8 +47,9 @@ public class User {
 		pstmt.setString(2, data.getUserPwd());
 		ResultSet rs = pstmt.executeQuery();
 		
-		String userTatus;
 		//결과 출력
+		String userTatus;
+		
 		if(rs.next()) {
 			userTatus = rs.getString("USER_STATUS");
 			Main.login_member_no = rs.getInt("USER_NO");
@@ -63,8 +59,8 @@ public class User {
 				String nick = rs.getString("NICK");
 				String balance = rs.getString("BALANCE");
 				String trustLevel = rs.getString("TRUST_LEVEL");
-				System.out.println("");
-				System.out.println(nick + " 님 환영합니다.");
+				
+				System.out.println("\n"+nick + " 님 환영합니다.");
 				System.out.println("매너온도	: "+ trustLevel);
 				if(balance != null) {
 					System.out.println("잔액	: "+ balance);
@@ -74,9 +70,9 @@ public class User {
 			}
 			
 			// 정지 계정
-			else if(userTatus.equals("Y")) {
-				System.out.println("");
-				System.out.println("정지된 회원입니다");
+			else if(userTatus.equals("S")) {
+				
+				System.out.println("\n정지된 회원입니다");
 				
 				sql = "SELECT BAN.STOP_REASON, BAN.RELEASE_DATE FROM BANNED BAN LEFT JOIN K_USER U ON (U.USER_NO = BAN.USER_NO) WHERE U.USER_NO = ?";
 				pstmt = conn.prepareStatement(sql);
@@ -90,26 +86,21 @@ public class User {
 					System.out.println("\n자동으로 로그아웃 됩니다.");
 					Main.login_member_no = 0;
 				}
-				
-				
-				
 			}
 			
 			// 탈퇴 계정
 			else if(userTatus.equals("Q")) {
-				System.out.println("탈퇴한 회원입니다");
+				System.out.println("\n탈퇴한 회원입니다");
 				System.out.println("\n자동으로 로그아웃 됩니다.");
 				Main.login_member_no = 0;
 			} 
-			
-		} else {
-			System.out.println("로그인 실패");
 		}
 		
+		return Main.login_member_no;
 	}
 	
 	// 아이디 찾기 (전화번호 , 힌트)
-	public void findId(Connection conn) throws SQLException {
+	public void findId(Connection conn) throws Exception {
 		
 		// 전화번호 입력 받기
 		String joinPhoneNo = uInput.phoneNoInput();
@@ -144,14 +135,14 @@ public class User {
 	}
 	
 	// 비밀번호 찾기(아이디 & 힌트)
-	public void findPwd(Connection conn) throws SQLException {
+	public void findPwd(Connection conn) throws Exception {
 		
 		// 정보입력 받기
 		System.out.print("가입시 입력한 아이디 : ");
 		String userId = Main.SC.nextLine();
 		String joinPhoneNo = uInput.phoneNoInput();
 		
-		String sql = "SELECT H.QUESTION FROM K_USER U LEFT JOIN HINT_TYPE H ON (U.QUESTION_NO = H.QUESTION_NO) WHERE ID = ? AND PHONE_NO = ?";
+		String sql = "SELECT H.QUESTION FROM K_USER U LEFT JOIN HINT_TYPE H ON (U.QUESTION_NO = H.QUESTION_NO) WHERE UPPER(ID) = UPPER( ? ) AND PHONE_NO = ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, userId);
 		pstmt.setString(2, joinPhoneNo);
@@ -161,7 +152,7 @@ public class User {
 		if(rs.next()) {
 			UserData data = uInput.findUserIdInput(joinPhoneNo,rs.getString("QUESTION"));
 			
-			sql = "SELECT U.PWD  FROM K_USER U LEFT JOIN HINT_TYPE H ON (U.QUESTION_NO = H.QUESTION_NO) WHERE ID = ? AND PHONE_NO = ? AND ANSWER = ?";
+			sql = "SELECT U.PWD  FROM K_USER U LEFT JOIN HINT_TYPE H ON (U.QUESTION_NO = H.QUESTION_NO) WHERE UPPER(ID) = UPPER( ? ) AND PHONE_NO = ? AND ANSWER = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userId);
 			pstmt.setString(2, data.getUserPhone());
@@ -169,6 +160,11 @@ public class User {
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
+				sql = "UPDATE K_USER SET PWD = ? WHERE USER_NO = ?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, userId);
+				pstmt.setString(2, data.getUserPhone());
+				
 				System.out.println("");
 				System.out.println("회원님의 비밀번호는 "+rs.getString("PWD")+"입니다.");
 			} else {
@@ -182,10 +178,12 @@ public class User {
 	}
 	
 	// 회원 탈퇴
-	public void dropUser(Connection conn) throws SQLException {
+	public int dropUser(Connection conn) throws Exception {
+		
+		int rrs = 0;
 		
 		System.out.println("정말로 탈퇴 하시겠습니까?");
-		System.out.println("1. 네 / 2. 아니오");
+		System.out.print("1. 네 / 2. 아니오 : ");
 		String yn = Main.SC.nextLine();
 		
 		if(yn.equals("1")) {
@@ -206,51 +204,47 @@ public class User {
 					sql = "UPDATE K_USER SET USER_STATUS = 'Q' WHERE USER_NO = ?";
 					pstmt = conn.prepareStatement(sql);
 					pstmt.setInt(1, Main.login_member_no);
-					int rrs = pstmt.executeUpdate();
+					rrs = pstmt.executeUpdate();
 					
 					if(rrs != 0) {
 						System.out.println("탈퇴처리를 완료했습니다.");
 						Main.login_member_no = 0;
 					}
-					else {
-						System.out.println("탈퇴처리를 실패했습니다.");
-					}
-				} else {
-					System.out.println("입력한 값이 다릅니다.");
-				}
+				} else { System.out.println("입력한 값이 다릅니다."); }
 			}
-			
 		}
-		else {
-			System.out.println("탈퇴 취소");
-		}
+		else { System.out.println("탈퇴 취소"); }
 		
+		return rrs; 
 	}
 	
 	// 질문하기
-	public void askQuestion(Connection conn) throws SQLException {
+	public int askQuestion(Connection conn) throws Exception {
 		
 		
 		//질문 받기
-		System.out.println("질문을 입력해 주세요");
+		System.out.print("질문을 입력해 주세요 : ");
 		String userQuestion = Main.SC.nextLine();
 		
-		String sql = "INSERT INTO QNA (QUESTION_NO,USER_NO,QUESTION,ANSWER) VALUES (SEQ_QNA_QUESTION_NO.NEXTVAL,?,?,?)";
+		String sql = "INSERT INTO QNA (QUESTION_NO,USER_NO,QUESTION) VALUES (SEQ_QNA_QUESTION_NO.NEXTVAL,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setInt(1, Main.login_member_no);
 		pstmt.setString(2, userQuestion);
-		pstmt.setString(3, "미답변");
 		int result = pstmt.executeUpdate();
 		
-		if(result == 1) {
-			System.out.println("질문 등록완료");
-		}
+		if(result == 1) { System.out.println("질문 등록완료"); }
+		
+		return result;
 	}
-	public void userList(Connection conn) throws SQLException {
+	
+	
+	public ResultSet userList(Connection conn) throws Exception {
+		
+		ResultSet rs;
 		
 		String sql = "SELECT * FROM K_USER";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		ResultSet rs = pstmt.executeQuery();
+		rs = pstmt.executeQuery();
 
 		while(rs.next()) {
 			String userNo = rs.getString("USER_NO");
@@ -264,6 +258,8 @@ public class User {
 			System.out.print(userNick);
 			System.out.println();
 		}
+		
+		return rs;
 	}
 	
 }
