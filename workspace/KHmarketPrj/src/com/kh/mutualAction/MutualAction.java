3626c14aec94d0775d805e770af4831d2e147e1f
이승권 <>
2023-02-22 16:51:01 +0900

    no message

--- workspace/KHmarketPrj/src/com/kh/mutualAction/MutualAction.java
+++ workspace/KHmarketPrj/src/com/kh/mutualAction/MutualAction.java
@@ -5,16 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.kh.item.BuyOrSell;
 import com.kh.main.Main;
 
 // 테이블 : 좋아요 목록, 거래 후기, 상품, 채팅방, 채팅 내용 
 
 public class MutualAction {
 	
+	BuyOrSell buyorsell=new BuyOrSell();
 	
 	public void setLikeList(Connection conn) throws Exception{
 		// 좋아요(찜) 하기 
-		//유저번호 1번이 상품번호 1번을 보고 찜을 눌러서 찜목록에 추가.
 		try {
 			System.out.println("상품 번호를 입력하세요 : ");
 			int item_no = Integer.parseInt(Main.SC.nextLine());
@@ -35,9 +36,6 @@ public class MutualAction {
 		} catch (Exception e) {
 			throw new Exception("이미 찜 한 상품입니다.");
 		}
-		
-		
-		
 		
 	}//method
 	
@@ -76,68 +74,174 @@ public class MutualAction {
 		}
 	}
 	
-	public void buyReview(Connection conn) throws Exception {
-		//구매 후기 작성
-		System.out.print("거래완료 한 상품 번호 : ");
-		int item_no = Integer.parseInt(Main.SC.nextLine());
+	public void sellItem(Connection conn) throws Exception {
+		// 판매등록
+		System.out.print("판매 등록 할 상품의 카테고리 번호를 입력하세요 : ");
+		int type_no = Integer.parseInt(Main.SC.nextLine());
 		
-		System.out.println("구매 후기 작성 : ");
+		System.out.print("상품의 제목을 입력하세요 : ");
+		String title = Main.SC.nextLine();
+
+		System.out.print("상품의 정보를 입력하세요 : ");
 		String content = Main.SC.nextLine();
 		
-		String sql = "INSERT INTO REVIEW(REVIEW_NO, USER_NO, ITEM_NO, BUY_REVIEW) VALUES (SEQ_REVIEW_NO,NEXTVAL,?,?,?)";
+		System.out.print("가격을 적어주세요 : ");
+		int price = Integer.parseInt(Main.SC.nextLine());
+		
+		String sql ="INSERT INTO ITEM(ITEM_NO,TYPE_NO,USER_NO,TITLE,CONTENT,PRICE,TRADE_STATUS) VALUES (SEQ_ITEM_NO.NEXTVAL,?,?,?,?,?,'N')";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, Main.login_member_no);
-		pstmt.setInt(2, item_no);
-		pstmt.setString(3, content);
+		pstmt.setInt(1, type_no);
+		pstmt.setInt(2, Main.login_member_no);
+		pstmt.setString(3, title);
+		pstmt.setString(4, content);
+		pstmt.setInt(5, price);
 		int result = pstmt.executeUpdate();
 		
 		if(result==1) {
-			System.out.println("후기가 작성되었습니다.");
+			System.out.println("판매등록이 완료되었습니다.");
+		}
+		else {
+			throw new Exception("등록이 실패했습니다.");
+		}
+	}
+	
+	
+	public void buyItem(Connection conn) throws Exception {
+		// 상품 확인후 구매
+		System.out.println("===========");
+		System.out.println("  상품 목록  ");
+		System.out.println("===========");
+		String sql ="SELECT * FROM ITEM WHERE TRADE_STATUS = 'N' ";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			int itemNo = rs.getInt("ITEM_NO");
+			int typeNo = rs.getInt("TYPE_NO");
+			String title = rs.getString("TITLE");
+			String content = rs.getString("CONTENT");
+			int price = rs.getInt("PRICE");
+			int view = rs.getInt("VIEW");
+			String tradeStatus = rs.getString("TRADE_STATUS");
+		
+			System.out.print("상품 번호: "+itemNo);
+			System.out.print(" | ");
+			System.out.print("카테고리: "+typeNo);
+			System.out.print(" | ");
+			System.out.print("제목: "+title);
+			System.out.print(" | ");
+			System.out.print("내용: "+content);
+			System.out.print(" | ");
+			System.out.println("가격: "+price);
+			System.out.print(" | ");
+			System.out.println("조회 수: "+view);
+			System.out.print(" | ");
+			System.out.println("거래 상태: "+tradeStatus);
+		}
+		
+		System.out.println("구매 하고싶은 상품의 번호를 입력하세요 : ");
+		int item_no = Integer.parseInt(Main.SC.nextLine());
+		
+		sql ="UPDATE ITEM SET TRADE_STATUS ='Y' WHERE ITEM_NO=? AND TRADE_STATUS ='N'";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, item_no);
+		int result = pstmt.executeUpdate();
+		
+		if(result==1) {
+			System.out.println("구매가 완료되었습니다.");
+		}
+		else {
+			throw new Exception("판매된 상품입니다.");
+		}
+	}
+	
+	public void buyReview(Connection conn) throws Exception {
+		//구매 후기 작성
+		System.out.print("거래완료한 상품 번호 : ");
+		int item_no = Integer.parseInt(Main.SC.nextLine());
+		
+		System.out.print("구매 후기를 작성해주세요: ");
+		String buyReview = Main.SC.nextLine();
+		
+		String sql = "INSERT INTO REVIEW(REVIEW_NO,USER_NO,ITEM_NO,BUY_REVIEW) VALUES(SEQ_REVIEW_NO.NEXTVAL,?,?,?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, Main.login_member_no);
+		pstmt.setInt(2, item_no);
+		pstmt.setString(3, buyReview);
+		int result = pstmt.executeUpdate();
+
+		if(result==1) {
+			System.out.println("후기작성이 완되었습니다.");
 		}
 		else {
 			throw new Exception("후기 작성 실패");
 		}
 	}
 	
-	public void sellReview() {
+	public void sellReview(Connection conn) throws Exception {
 		//판매 후기 작성
-		
-	}
-	
-	public void writeReview(Connection conn) throws Exception {
-		// 거래 후기 작성 
-		
-		System.out.println("거래완료한 상품의 번호 : ");
+		System.out.print("거래완료한 상품의 번호 : ");
 		int item_no = Integer.parseInt(Main.SC.nextLine());
 		
-		System.out.println("구매 후기를 입력해주세요 :");
-		String buyReview = Main.SC.nextLine();
-
-		System.out.println("판매 후기를 입력해주세요 :");
+		System.out.print("판매 후기를 작성해주세요: ");
 		String sellReview = Main.SC.nextLine();
 		
-		String sql = "INSERT INTO REVIEW(REVIEW_NO,USER_NO , ITEM,NO , BUY_REVIEW , SELL_REVIEW) VALUES (SEQ_REVIEW_NO.NEXTVAL,?,?,?,?)";
+		String sql = "INSERT INTO REVIEW(REVIEW_NO,USER_NO,ITEM_NO,SELL_REVIEW) VALUES(SEQ_REVIEW_NO.NEXTVAL,?,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setInt(1, Main.login_member_no);
 		pstmt.setInt(2, item_no);
-		pstmt.setString(3, buyReview);
-		pstmt.setString(4, sellReview);
+		pstmt.setString(3, sellReview);
+		int result = pstmt.executeUpdate();
+		
+		if(result == 1 ) {
+			System.out.println("후기작성이 완료되었습니다.");
+		}
+		else {
+			throw new Exception("후기 작성 실패");
+		}
 	}
 	
-	public void sellItem() {
-		// 판매
-	}
-	
-	public void buyItem() {
-		// 구매 
-	}
-	
-	public void makeChatRoom(Connection conn, int item_no , int user_no ) throws Exception {
+	public void makeChatRoom(Connection conn) throws Exception {
 		//채팅방 개설
-		String sql = "INSERT INTO CHAT_ROOM(ROOM_NO,ITEM_NO,USER_NO) VALUES (SEQ_ROOM_NO.NEXTVAL,?,?)";
+		// 마음에 드는 상품을 보고 채팅방을 생성
+		System.out.println("===========");
+		System.out.println("  상품 목록  ");
+		System.out.println("===========");
+		String sql ="SELECT * FROM ITEM WHERE TRADE_STATUS = 'N' ";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, user_no);
-		pstmt.setInt(2, item_no);
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			int itemNo = rs.getInt("ITEM_NO");
+			int typeNo = rs.getInt("TYPE_NO");
+			String title = rs.getString("TITLE");
+			String content = rs.getString("CONTENT");
+			int price = rs.getInt("PRICE");
+			int view = rs.getInt("VIEW");
+			String tradeStatus = rs.getString("TRADE_STATUS");
+		
+			System.out.print("상품 번호: "+itemNo);
+			System.out.print(" | ");
+			System.out.print("카테고리: "+typeNo);
+			System.out.print(" | ");
+			System.out.print("제목: "+title);
+			System.out.print(" | ");
+			System.out.print("내용: "+content);
+			System.out.print(" | ");
+			System.out.println("가격: "+price);
+			System.out.print(" | ");
+			System.out.println("조회 수: "+view);
+			System.out.print(" | ");
+			System.out.println("거래 상태: "+tradeStatus);
+		}
+		
+		System.out.println("상품을 선택하세요 : ");
+		int item_no = Integer.parseInt(Main.SC.nextLine());
+		
+		sql = "INSERT INTO CHAT_ROOM(ROOM_NO,ITEM_NO,USER_NO) VALUES (SEQ_ROOM_NO.NEXTVAL,?,?)";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, item_no);
+		pstmt.setInt(2, Main.login_member_no);
 		int result = pstmt.executeUpdate();
 		
 		if(result==1) {
@@ -149,15 +253,86 @@ public class MutualAction {
 		
 	}
 	
-	public void chating(Connection conn, int user_no) throws Exception {
-		// 채팅하기 
-		System.out.println("메시지를 입력하세요 : ");
-		String content = Main.SC.nextLine();
+	public void SearchChatRoomByUserNumber(Connection conn) throws Exception {
+		// 유저번호로 채팅방 목록 조회하는 메소드
+		System.out.print("찾으시려는 채팅방에 유저번호를 입력하세요 : ");
+		int user_no = Integer.parseInt(Main.SC.nextLine());
 		
-		String sql ="INSERT INTO CHAT_CONTENT(CHAT_NO, ROOM_NO , USER_NO , CONTENT, READ_YN) VALUES (SEQ_CHAT_NO.NEXTVAL , SEQ_ROOM_NO.NEXTVAL , ?, ?, 'Y')";
+		String sql ="SELECT ROOM_NO,ITEM_NO,USER_NO FROM CHAT_ROOM WHERE USER_NO =? ";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setInt(1, user_no);
-		pstmt.setString(2, content);
-	}
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			int roomNo = rs.getInt("ROOM_NO");
+			int itemNo = rs.getInt("ITEM_NO");
+			int userNo = rs.getInt("USER_NO");
+			
+			System.out.println("-------------------------");
+			System.out.println("|	채팅방 번호 : "+roomNo+"	|");
+			System.out.println("|	상품 번호 : "+itemNo+"	|");
+			System.out.println("|	유저 번호 : "+userNo+"	|");
+			System.out.println("-------------------------");
+			
+			chating(conn);
+		}
+		
+		
+	}//method
+	
+	public void SearchChatRoomByItemNumber(Connection conn) throws Exception {
+		// 상품번호로 채팅방 목록 조회하는 메소드
+		System.out.println();
+		System.out.print("찾으시려는 채팅방에 상품번호를 입력하세요 : ");
+		int item_no = Integer.parseInt(Main.SC.nextLine());
+		
+		String sql ="SELECT ROOM_NO,ITEM_NO,USER_NO FROM CHAT_ROOM WHERE ITEM_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, item_no);
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			int roomNo = rs.getInt("ROOM_NO");
+			int itemNo = rs.getInt("ITEM_NO");
+			int userNo = rs.getInt("USER_NO");
+			
+			System.out.println("-------------------------");
+			System.out.println("|	채팅방 번호 : "+roomNo+"	|");
+			System.out.println("|	상품 번호 : "+itemNo+"	|");
+			System.out.println("|	유저 번호 : "+userNo+"	|");
+			System.out.println("-------------------------");
+			
+			chating(conn);
+		}
+		
+	}//method
+	
+	public void chating(Connection conn) throws Exception {
+		// 채팅방 번호를 입력하여 들어가서 채팅하기 
+		System.out.print("채팅방 번호를 선택해주세요 : ");
+		int input = Integer.parseInt(Main.SC.nextLine());
+		System.out.println();
+		System.out.println("채팅방에 입장하셨습니다.");
+		
+		while(true) {
+			System.out.print("메시지를 입력하세요 : ");
+			String content = Main.SC.nextLine();
+
+			String sql ="INSERT INTO CHAT_CONTENT(CHAT_NO, ROOM_NO , USER_NO , CONTENT, READ_YN) VALUES (SEQ_CHAT_NO.NEXTVAL,?  , ?, ?, 'Y')";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, input);
+			pstmt.setInt(2, Main.login_member_no);
+			pstmt.setString(3, content);
+			
+			pstmt.executeUpdate();
+			
+			if(!content.equals("EXIT")) {
+				System.out.println("나가기 : EXIT");
+			}
+			else {
+				throw new Exception("채팅방에서 나갔습니다.");
+			}
+		}
+	}//method
 	
 }
--- workspace/KHmarketPrj/src/com/kh/mutualAction/MutualActionService.java
+++ workspace/KHmarketPrj/src/com/kh/mutualAction/MutualActionService.java
@@ -11,11 +11,19 @@ public class MutualActionService {
 	public void mutualActionPage(Connection conn) throws Exception {
 		
 		System.out.println("=============");
-		System.out.println("마이페이지");
+		System.out.println("   마이페이지   ");
 		System.out.println("=============");
 		System.out.println("1. 좋아요 등록하기 ");
 		System.out.println("2. 좋아요 삭제하기 ");
 		System.out.println("3. 좋아요 조회하기 ");
+		System.out.println("4. 상품 판매 ");
+		System.out.println("5. 상품 구매 ");
+		System.out.println("6. 구매 후기 작성 ");
+		System.out.println("7. 판매 후기 작성 ");
+		System.out.println("8. 채팅방 개설 ");
+		System.out.println("9. 유저번호로 채팅방 조회후 채팅하기 ");
+		System.out.println("10. 상품번호로 채팅방 조회후 채팅하기 ");
+		System.out.print("실행할 서비스 번호를 입력하세요 : ");
 		
 		String input = Main.SC.nextLine();
 		
@@ -24,7 +32,13 @@ public class MutualActionService {
 		case "1" : ma.setLikeList(conn); break;
 		case "2" : ma.deleteLikeList(conn); break;
 		case "3" : ma.selectLikeList(conn); break;
-		case "4" : ma.writeReview(conn);
+		case "4" : ma.sellItem(conn); break;
+		case "5" : ma.buyItem(conn); break;
+		case "6" : ma.buyReview(conn);	break;
+		case "7" : ma.sellReview(conn); break;
+		case "8" : ma.makeChatRoom(conn); break;
+		case "9" : ma.SearchChatRoomByUserNumber(conn); break;
+		case "10" : ma.SearchChatRoomByItemNumber(conn); break;
 		
 		}
 		
